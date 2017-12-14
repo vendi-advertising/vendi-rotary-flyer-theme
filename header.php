d0a34cc97d9169c741decddc5ba200f5e073cb77
Cdetviler <charlie@vendiadvertising.com>
2017-12-13 21:06:07 -0600

    added temporary redirect so that outside users could not access internal pages

--- header.php
+++ header.php
@@ -3,12 +3,11 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <?php
-// Probably not needed anymore
-// $user = wp_get_current_user();
-// if(!is_front_page() && 0 == $user->ID )
-// {
-//     auth_redirect();
-// }
+$user = wp_get_current_user();
+if(!is_front_page() && 0 == $user->ID )
+{
+     auth_redirect();
+ }
 
 wp_head();
 
